83564a890e839cece2b13344d28538142f07765c
akmalovichdev <akmalovichdev@yandex.com>
2025-10-06 15:47:33 +0300

    app

---
+++ bot/handlers/start.py
@@ -0,0 +1,23 @@
+import logging
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+from bot import dp
+from bot.keyboards.inlineKeyboards import CommandsMenu
+
+logger = logging.getLogger(__name__)
+
+# Команда /start
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message, state: FSMContext, is_new_user: bool = False):
+    """Обработчик команды /start"""
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+
+    # Приветственное сообщение
+    if is_new_user:
+            text = f"""
+
+"""
+
+    await message.reply(text, parse_mode='HTML')
+    await state.finish()
---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+import logging.config
+import os
+import asyncio
+from aiogram import executor
+from data.logs import LOGGING_CONFIG
+from data import db
+from bot import dp
+from bot import handlers  # noqa: F401  # регистрируем обработчики
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
